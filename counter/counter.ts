5bf69a83713f5d29acf7acd797e7cab4303afc50
Sreelakshmi Transight <sreelakshmitransight@gmail.com>
2025-10-06 14:12:38 +0530

    initialize project

---
+++ counter/counter.ts
@@ -0,0 +1,13 @@
+import { Component, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  imports: [],
+  templateUrl: './counter.html',
+  styleUrl: './counter.scss'
+})
+export class Counter {
+
+  count = signal(0);
+
+}
